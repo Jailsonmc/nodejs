cbf21e2c009f05501586d583bcf2fff724d23293
Jailson Cavalcanti <jailsoncavalcanti@Jailsons-Air-3.home>
2019-07-29 07:02:26 +0100

    Add port 80

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const http = require('http');
 
 // These could (should) be set as env vars.
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 const host = process.env.HOST || 'localhost';
 
 // No matter what hits the server, we send the same thing.
@@ -17,7 +17,7 @@ http.createServer((req, res) => {
   });
 
   // Send a simple message in HTML.
-  res.write(`<h1>Jailson's Site</h1>`);
+  res.write('<h1>Jailson</h1>');
   res.write('<h1>I’m a Node app!</h1>');
   res.write('<p>And I’m <em>sooooo</em> secure.</p>');
   res.end();
